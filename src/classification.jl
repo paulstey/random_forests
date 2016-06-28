6f2c41f8a468a15abb619d901d61e1bbaf29ea2f
paulstey <paulstey@gmail.com>
2016-06-28 14:58:47 -0400

    update measures.jl and classification.jl

--- src/classification.jl
+++ src/classification.jl
@@ -3,20 +3,22 @@
 function _split_classifcation_error_loss(y::Vector, X::Matrix, weights::Vector)
     best = NO_BEST
     best_val = -Inf
-    n, p = size(X)
+    p = size(X, 2)
 
     for j in 1:p
-        x_j = X[:, j]
+        keep_row = !isna(X[:, j])
+        x_obs = convert(Vector, X[keep_row, j])
+        y_obs = y[keep_row]
 
-        if length(unique(x_j)) ≥ 100
-            domain_j = quantile(x_j, linspace(0.01, 0.99, 99))
+        if length(unique(x_obs)) ≥ 100
+            domain_j = quantile(x_obs, linspace(0.01, 0.99, 99))
         else
-            domain_j = sort(unique(x_j))
+            domain_j = sort(unique(x_obs))
         end
 
         for thresh in domain_j[2:end]
 
-            cur_split = x_j .< thresh
+            cur_split = x_obs .< thresh
             value = _classifcation_error_loss(y[cur_split], weights[cur_split]) + _classifcation_error_loss(y[!cur_split], weights[!cur_split])
 
             if value > best_val
